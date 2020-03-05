52d36252a047667579619d80544e4c126cbf3d28
Wesley Santos <santoswesley1998@outlook.com>
2020-03-05 02:40:08 -0300

    image hosting

--- src/routes/HomeRoute/UserPage.tsx
+++ src/routes/HomeRoute/UserPage.tsx
@@ -145,19 +145,22 @@ export default function UserPage({ match }) {
 
       const data = new FormData(e.currentTarget);
       const imageFile = data.get("image") || null;
+      const name = data.get("name") || null;
+      const username = data.get("username") || null;
+      const email = data.get("email") || null;
 
       if (imageFile) {
         const reader = new FileReader();
 
         reader.onload = () => {
-          fetch("", {
+          fetch("https://192.168.0.25:7001", {
             method: "POST",
             body: reader.result,
           }).then(res => res.text().then(imageUrl => {
             api.updateUser(localState.user.username, {
-              name: data.get("name"),
-              username: data.get("username"),
-              email: data.get("email"),
+              name: name != localState.user.name ? name : null,
+              username: username != localState.user.username ? username : null,
+              email: email != localState.user.email ? email : null,
               imageUrl
             }).then(user => {
               dispath({
@@ -173,9 +176,9 @@ export default function UserPage({ match }) {
         reader.readAsArrayBuffer(imageFile);
       } else {
         api.updateUser(localState.user.username, {
-          name: data.get("name"),
-          username: data.get("username"),
-          email: data.get("email"),
+            name: name != localState.user.name ? name : null,
+            username: username != localState.user.username ? username : null,
+            email: email != localState.user.email ? email : null,
         }).then(user => {
           dispath({
             type: "UPDATE_USER",
@@ -185,6 +188,24 @@ export default function UserPage({ match }) {
           });
         });
       }
+    }
+
+    function passwordFormSubmitHandler(e: React.FormEvent<HTMLFormElement>) {
+      e.preventDefault();
+
+      const data = new FormData(e.currentTarget);
+
+      api.updateUser(localState.user.username, {
+        password: data.get("password"),
+        currentPassword: data.get("currentPassword"),
+      }).then(user => {
+        dispath({
+          type: "UPDATE_USER",
+          data: {
+            user,
+          }
+        });
+      });
     }
 
     function fileInputHandler(e: React.FormEvent<HTMLInputElement>) {
@@ -206,7 +227,9 @@ export default function UserPage({ match }) {
 
     return (
       <React.Fragment>
-        <form className="p-3" onSubmit={formSubmitHandler}>
+        <form
+          className="p-3"
+          onSubmit={formSubmitHandler}>
           <div className="form-row">
             <label className="col-2 pointer">
               <img
@@ -227,11 +250,12 @@ export default function UserPage({ match }) {
                 name="name"
                 className="form-control form-control-sm"
                 defaultValue={localState.user.name}
+                required
                 aria-describedby="nameHelp"/>
               <small
                 id="nameHelp"
                 className="form-text text-muted">
-                {"We'll never share your email with anyone else."}
+                {t("desc.name")}
               </small>
             </div>
           </div>
@@ -243,11 +267,12 @@ export default function UserPage({ match }) {
               name="username"
               className="form-control form-control-sm"
               defaultValue={localState.user.username}
+              required
               aria-describedby="usernameHelp"/>
             <small
               id="usernameHelp"
               className="form-text text-muted">
-              {"We'll never share your email with anyone else."}
+              {t("desc.username")}
             </small>
           </div>
           <div className="form-group">
@@ -258,11 +283,12 @@ export default function UserPage({ match }) {
               name="email"
               className="form-control form-control-sm"
               defaultValue={localState.user.email}
+              required
               aria-describedby="emailHelp"/>
             <small
               id="emailHelp"
               className="form-text text-muted">
-              {"We'll never share your email with anyone else."}
+              {t("dest.email")}
             </small>
           </div>
           <button
@@ -273,15 +299,20 @@ export default function UserPage({ match }) {
         </form>
         <hr/>
         <h6 className="px-3">
-          <b>Change password</b>
+          <b>{t("security")}</b>
         </h6>
-        <form className="p-3" onSubmit={formSubmitHandler}>
+        <form
+          className="p-3"
+          onSubmit={passwordFormSubmitHandler}>
           <div className="form-group">
             <label for="usernameInput">{t("currentPassword")}</label>
             <input
               type="password"
               id="currentPasswordInput"
               name="currentPassword"
+              pattern=".{8,100}"
+              required
+              autocomplete="current-password"
               className="form-control form-control-sm"/>
           </div>
           <div className="form-group">
@@ -291,6 +322,8 @@ export default function UserPage({ match }) {
               id="passwordInput"
               name="password"
               pattern=".{8,100}"
+              required
+              autocomplete="new-password"
               className="form-control form-control-sm"/>
           </div>
           <button
